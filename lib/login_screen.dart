0ee3ddace1566df429d6b00bc243cfa8e8dd0096
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-11 03:43:06 +0530

    Initial commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  //loading delay
+  Duration get loadingTime => const Duration(seconds: 3);
+
+  //login
+  Future<String?> _authUser(LoginData data){
+    return Future.delayed(loadingTime).then((value) => null);
+  }
+
+  //forget password
+  Future<String?> _recoverPassword(String data){
+    return Future.delayed(loadingTime).then((value) => null);
+  }
+
+  //signup user
+  Future<String?> _signupUser(SignupData data){
+    return Future.delayed(loadingTime).then((value) => null);
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: FlutterLogin(
+          onLogin: _authUser,
+          onRecoverPassword: _recoverPassword,
+        onSignup: _signupUser,
+      ),
+    );
   }
 }
